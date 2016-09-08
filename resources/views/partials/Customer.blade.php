61e5f16718d2bcb99889165a17ff6f59f1f667ee
XinhNguyen <nguyentrucxjnh@gmail.com>
2016-09-08 14:56:09 +0700

    finish layout crud

--- resources/views/partials/Customer.blade.php
+++ resources/views/partials/Customer.blade.php
@@ -1,74 +1,30 @@
 <style>
-    .menu-open .menu-side {
-        right: 0;
+    .btn-del-edit {
+        float: left;
+        padding-right: 5px
     }
-    .btn-del-edit{
-        float: left;padding-right: 5px
-    }
-    .menu-side {
-        -webkit-transition: right 0.5s ease;
-        -moz-transition: right 0.5s ease;
-        transition: right 0.5s ease;
-    }
-
-    .menu-side {
-        background-color: #333;
-        border-left: 1px solid #000;
-        color: #fff;
+    #frmControl {
+        z-index: 1000;
         position: fixed;
-        top: 0;
-        right: -431px;
-        width: 400px;
-        height: 100%;
-        padding: 70px 10px 10px 10px;
-        z-index:2;
+        top: 200px;
+        display: none;
+        right: 10px;
     }
 </style>
 
-<div class="row" onwheel="mouseScroll()">
+<div class="row">
     <div class="col-md-12">
         <div class="row">
             <div class="col-lg-12">
                 <h5 style="color:#2196f3">Quản lý khách hàng
                     <div class="pull-right menu-toggle">
-                        <button type="button" class="btn btn-primary btn-ms">Thêm mới</button>
+                        <button onclick="show()" type="button" class="btn btn-primary btn-sm btn-circle"><i class="fa fa-plus" aria-hidden="true"></i></button>
                     </div>
-               </h5>
+                </h5>
                 <hr style="margin-top: 0px;">
             </div>
             <!-- /.col-lg-12 -->
         </div>
-
-        <header>
-            <nav class="menu-side">
-                <div class="menu-toggle">
-                    <i class="glyphicon glyphicon-remove"></i>
-                </div>
-              <form>
-
-                    <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                        <label for="exampleInputFile">File input</label>
-                        <input type="file" id="exampleInputFile">
-                        <p class="help-block">Example block-level help text here.</p>
-                    </div>
-                    <div class="checkbox">
-                        <label>
-                            <input type="checkbox"> Check me out
-                        </label>
-                    </div>
-                    <button type="submit" class="btn btn-default">Submit</button>
-                </form>
-            </nav>
-        </header>
-
 
         <div class="table-responsive">
             <table class="table table-bordered table-hover order-column" id="tableUserList"
@@ -80,7 +36,7 @@
                     <th>Số lít</th>
                     <th>Đơn giá</th>
                     <th>Tổng chi phí</th>
-                    <th>Xóa / Sửa</th>
+                    <th>Sửa/ Xóa</th>
                 </tr>
                 </thead>
                 <tbody id="tbodyUserList">
@@ -92,36 +48,53 @@
                     <td>aaaa</td>
                     <td>
                         <div class="menu-toggle btn-del-edit">
+                            <div class="btn btn-success  btn-circle">
+                                <i class="glyphicon glyphicon-pencil"></i>
+                            </div>
+                        </div>
+                        <div class="menu-toggle">
                             <div class="btn btn-danger btn-circle">
                                 <i class="glyphicon glyphicon-remove"></i>
                             </div>
                         </div>
-                        <div class="menu-toggle">
-                            <div class="btn btn-danger  btn-circle">
-                                <i class="glyphicon glyphicon-pencil"></i>
-                            </div>
-                        </div>
-                    </td>
-                  </tr>
-                </tbody>
-        </table>
-    </div>
-</div>
-</div>
-<script>
-    (function () {
-        var header = $('header');
-        $('.menu-toggle').bind('click', function () {
-            header.toggleClass('menu-open');
-            return false;
-        });
-    })();
 
-    function mouseScroll() {
-        if (document.body.scrollTop > 70) {
-            $('nav.menu-side').css('padding-top', '0px');
-        } else {
-            $('nav.menu-side').css('padding-top', '50px');
-        }
+                    </td>
+                </tr>
+
+                </tbody>
+            </table>
+
+        </div> <!-- end table-reposive -->
+        <div id="frmControl" class="col-md-offset-4 col-md-8">
+            <div class="panel panel-primary">
+                <div class="panel-heading">Panel heading without title</div>
+                <div class="panel-body">
+                    <form action="" method="post" role="form">
+                        <div class="form-group">
+                            <label for=""></label>
+                            <input type="text" class="form-control" name="" id="" placeholder="Input...">
+                        </div>
+                        <div class="form-group">
+                            <label for=""></label>
+                            <input type="text" class="form-control" name="" id="" placeholder="Input...">
+                        </div>
+                        <div class="form-group">
+                            <label for=""></label>
+                            <input type="text" class="form-control" name="" id="" placeholder="Input...">
+                        </div>
+                        <div class="form-group">
+                            <label for=""></label>
+                            <input type="text" class="form-control" name="" id="" placeholder="Input...">
+                        </div>
+                        <button type="submit" class="btn btn-primary pull-right">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div> <!-- end #frmControl -->
+    </div> <!-- end .col-md-12-->
+</div> <!-- end .row -->
+<script>
+    function show() {
+        $('#frmControl').fadeToggle();
     }
 </script>
