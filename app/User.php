ddc8e017bb20e11ec665d0209e8a349887c2851d
XinhNguyen <nguyentrucxjnh@gmail.com>
2016-09-12 09:02:49 +0700

    add fieldname 'active' to users table

--- app/User.php
+++ app/User.php
@@ -14,7 +14,8 @@ class User extends Authenticatable
     protected $fillable = [
         'username', 
         'email', 
-        'password'
+        'password',
+        'active'
     ];
 
     /**
