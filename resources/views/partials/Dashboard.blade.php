94a2fad5eb11843be2f12c7d834ffade56fc3a81
nvnghia <nvnghia0202@gmail.com>
2016-11-23 14:04:05 +0700

    fix chart

--- resources/views/partials/Dashboard.blade.php
+++ resources/views/partials/Dashboard.blade.php
@@ -18,120 +18,8 @@
             dashboardView = {
                 loadChart1: function () {
                     $('#chart1').highcharts({
-                        chart: {
-                            type: 'column'
-                        },
-
                         title: {
-                            text: 'Biểu đồ thống kê lượt vận chuyển theo tháng'
-                        },
-
-                        xAxis: {
-                            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
-                        },
-
-                        yAxis: {
-                            allowDecimals: false,
-                            min: 0,
-                            title: {
-                                text: 'Số lượt trên tháng'
-                            }
-                        },
-
-                        tooltip: {
-                            formatter: function () {
-                                return '<b>' + this.x + '</b><br/>' +
-                                        this.series.name + ': ' + this.y + '<br/>' +
-                                        'Total: ' + this.point.stackTotal;
-                            }
-                        },
-
-                        plotOptions: {
-                            column: {
-                                stacking: 'normal'
-                            }
-                        },
-
-                        series: [
-                            {
-                                name: 'John',
-                                data: [5, 3, 4, 7, 2],
-                                stack: 'male'
-                            }, {
-                                name: 'Joe',
-                                data: [3, 4, 4, 2, 5],
-                                stack: 'male'
-                            }, {
-                                name: 'Jane',
-                                data: [2, 5, 6, 2, 1],
-                                stack: 'female'
-                            }, {
-                                name: 'Janet',
-                                data: [3, 0, 4, 4, 3],
-                                stack: 'female'
-                            }
-                        ]
-                    });
-                },
-                loadChart2: function () {
-                    $('#chart2').highcharts({
-                        chart: {
-                            type: 'area'
-                        },
-                        title: {
-                            text: 'Biểu đồ thống kê chi phí theo tháng'
-                        },
-                        xAxis: {
-                            categories: ['1750', '1800', '1850', '1900', '1950', '1999', '2050'],
-                            tickmarkPlacement: 'on',
-                            title: {
-                                enabled: false
-                            }
-                        },
-                        yAxis: {
-                            title: {
-                                text: 'Triệu đồng'
-                            },
-                            labels: {
-                                formatter: function () {
-                                    return this.value / 1000;
-                                }
-                            }
-                        },
-                        tooltip: {
-                            shared: true,
-                            valueSuffix: ' triệu đồng'
-                        },
-                        plotOptions: {
-                            area: {
-                                stacking: 'normal',
-                                lineColor: '#666666',
-                                lineWidth: 1,
-                                marker: {
-                                    lineWidth: 1,
-                                    lineColor: '#666666'
-                                }
-                            }
-                        },
-                        series: [{
-                            name: 'Nhiên liệu',
-                            data: [502, 635, 809, 947, 1402, 3634, 5268]
-                        }, {
-                            name: 'Thay nhớt',
-                            data: [106, 107, 111, 133, 221, 767, 1766]
-                        }, {
-                            name: 'Đậu bãi',
-                            data: [163, 203, 276, 408, 547, 729, 628]
-                        }, {
-                            name: 'Khác',
-                            data: [18, 31, 54, 156, 339, 818, 1201]
-                        }]
-                    });
-                },
-                loadChart3: function () {
-                    $('#chart3').highcharts({
-                        title: {
-                            text: 'Thống kê / So sánh lợi nhuận qua các năm',
+                            text: 'Lượt vận chuyển',
                             x: -20 //center
                         },
                         xAxis: {
@@ -140,7 +28,7 @@
                         },
                         yAxis: {
                             title: {
-                                text: 'Triệu đồng'
+                                text: 'Lượt'
                             },
                             plotLines: [{
                                 value: 0,
@@ -149,7 +37,7 @@
                             }]
                         },
                         tooltip: {
-                            valueSuffix: ' triệu đồng'
+                            valueSuffix: ' Lượt'
                         },
                         legend: {
                             layout: 'vertical',
@@ -160,16 +48,101 @@
                         series: [{
                             name: '2016',
                             data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-                        }, {
-                            name: '2015',
-                            data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-                        }, {
-                            name: '2014',
-                            data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-                        }, {
-                            name: '2013',
-                            data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
                         }]
+                    });
+                },
+                loadChart2: function () {
+                    $('#chart2').highcharts({
+                        chart: {
+                            type: 'column'
+                        },
+
+                        title: {
+                            text: 'Doanh thu và lợi nhuận'
+                        },
+
+                        xAxis: {
+                            categories: ['Tháng 1', 'Tháng 2', 'Tháng 3', 'Tháng 4', 'Tháng 5', 'Tháng 6',
+                                'Tháng 7', 'Tháng 8', 'Tháng 9', 'Tháng 10', 'Tháng 11', 'Tháng 12']
+                        },
+
+                        yAxis: {
+                            allowDecimals: false,
+                            min: 0,
+                            title: {
+                                text: ' Triệu đồng'
+                            }
+                        },
+                        tooltip: {
+                            valueSuffix: ' Triệu đồng'
+                        },
+                        plotOptions: {
+                        },
+
+                        series: [
+                            {
+                                name: 'Doanh thu',
+                                data: [5, 3, 4, 7, 2,5, 3, 4, 7, 2,4,5],
+                            }, {
+                                name: 'Lợi nhuận',
+                                data: [3, 4, 4, 2, 5,3, 4, 4, 2, 5,4,9],
+                            }
+                        ]
+                    });
+                },
+                loadChart3: function () {
+                    $('#chart3').highcharts({
+                        title: {
+                            text: 'Lợi nhuận',
+                            x: -20 //center
+                        },
+                        xAxis: {
+                            categories: [
+                                'Tháng 1', 
+                                'Tháng 2', 
+                                'Tháng 3', 
+                                'Tháng 4', 
+                                'Tháng 5', 
+                                'Tháng 6',
+                                'Tháng 7', 
+                                'Tháng 8', 
+                                'Tháng 9', 
+                                'Tháng 10', 
+                                'Tháng 11', 
+                                'Tháng 12'
+                            ]
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Triệu đồng'
+                            },
+                            plotLines: [
+                                {
+                                    value: 0,
+                                    width: 1,
+                                    color: '#808080'
+                                }
+                            ]
+                        },
+                        tooltip: {
+                            valueSuffix: ' triệu đồng'
+                        },
+                        legend: {
+                            layout: 'vertical',
+                            align: 'right',
+                            verticalAlign: 'middle',
+                            borderWidth: 0
+                        },
+                        series: [
+                            {
+                                name: 'Năm 2015',
+                                data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
+                            }, 
+                            {
+                                name: 'Năm 2016',
+                                data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
+                            }
+                        ]
                     });
                 }
             };
